7d2ade3529fa03526b567ee4b84314e2a6e056e3
Yohann <yohannjaffres88@gmail.com>
2024-04-23 13:05:32 +0200

    remove useless controller

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,13 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppService } from './app.service';
-import { CatsController } from './cats/cats.controller';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 import { AuthController } from './auth/auth.controller';
 
 @Module({
   imports: [AuthModule, UsersModule],
-  controllers: [CatsController, AuthController],
+  controllers: [AuthController],
   providers: [AppService],
 })
 export class AppModule {}
